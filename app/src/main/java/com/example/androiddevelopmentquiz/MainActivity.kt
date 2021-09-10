05161633b46cd6e5cdb02a961d75d40868a92a15
Ayrton <ayrttoon@hotmail.com>
2021-09-10 18:54:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androiddevelopmentquiz/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.androiddevelopmentquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+}
